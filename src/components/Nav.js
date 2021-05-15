fe96d70e22e884bce35c039b98ad0467f22b5928
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2021-05-15 22:04:11 +0530

    retrying comments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,11 +27,11 @@ function Nav() {
           src="https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png"
           alt="Netflix"
         />
-//         <img
-//           className="nav__avatar"
-//           src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-//           alt=""
-//         />
+{/*<img
+           className="nav__avatar"
+           src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+           alt=""
+         />*/}
       </div>
     </div>
   );
